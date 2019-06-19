0358a2c51a718856828b3c89065af49191fb3e7f
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-06-19 13:49:18 -0300

    Update Arduino_ethernetHTTPServer.ino

--- Arduino_ethernetHTTPServer/Arduino_ethernetHTTPServer.ino
+++ Arduino_ethernetHTTPServer/Arduino_ethernetHTTPServer.ino
@@ -21,7 +21,8 @@ void setup()
 
 	// Open serial communications and wait for port to open:
 	Serial.begin(9600);
-	while(!Serial){}
+	while(!Serial) delay(5);
+
 	Serial.println("Ethernet WebServer Example");
 
 	// start the Ethernet connection and the server:
@@ -30,13 +31,10 @@ void setup()
 	// Check for Ethernet hardware present
 	if(Ethernet.hardwareStatus() == EthernetNoHardware){
 		Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-		while(true){
-			delay(1); // do nothing, no point running without Ethernet hardware
-		}
+		while(true) delay(5); // do nothing, no point running without Ethernet hardware
 	}
-	if(Ethernet.linkStatus() == LinkOFF){
+	if(Ethernet.linkStatus() == LinkOFF)
 		Serial.println("Ethernet cable is not connected.");
-	}
 
 	// Arduino UNO
 	// pinMode(0, OUTPUT);
